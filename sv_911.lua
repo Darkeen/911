40bc6ed1cc35e63ceda9f641fd8edf68da57b4c1
ChezzaGB <36545043+ChezzaGB@users.noreply.github.com>
2019-03-14 17:14:46 +0000

    Add files via upload

--- sv_911.lua
+++ sv_911.lua
@@ -1,8 +1,14 @@
 -- Simple 911 Command (With Location) -- 
 		-- Made By Chezza --
 
+displayLocation = true
+
 RegisterServerEvent('911')
 AddEventHandler('911', function(location, msg)
     local playername = GetPlayerName(source)
-   	TriggerClientEvent('chatMessage', -1, '', {255,255,255}, '^*^4911 | Caller ID: ^r' .. playername .. '^*^4 | Location: ^r' .. location .. '^*^4 | Report: ^r' .. msg)
+    if displayLocation == false then
+   		TriggerClientEvent('chatMessage', -1, '', {255,255,255}, '^*^4911 | Caller ID: ^r' .. playername .. '^*^4 | Report: ^r' .. msg)
+   	else
+   		TriggerClientEvent('chatMessage', -1, '', {255,255,255}, '^*^4911 | Caller ID: ^r' .. playername .. '^*^4 | Location: ^r' .. location .. '^*^4 | Report: ^r' .. msg)
+   	end
 end)
